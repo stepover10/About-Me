a7dc273e54c54f1957d5308d83a456a85839971f
stepover10 <stepover10@gmail.com>
2019-01-15 15:11:06 +0900

    ssr_test

--- reactjs/dev-server-side-rendering/src/main/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Layout from './Layout'
-
-
-if (typeof window !== 'undefined') {
-    ReactDOM.hydrate(<Layout />, document.getElementById('app'))
-}
